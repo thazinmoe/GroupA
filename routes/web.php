c123de3651b18ced263d0131c85ed3003e358db2
thazinmoe <thazinmoe1097@gmail.com>
2022-08-15 20:29:40 +0630

    categories-controller-add

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\PageController::class, 'home'])->name('home');
+Route::get('posts', [App\Http\Controllers\PageController::class, 'posts'])->name('posts');
+Route::get('posts/{post:slug}', [App\Http\Controllers\PageController::class, 'detailPost'])->name('posts.show');
+Route::get('paket-travel', [App\Http\Controllers\PageController::class, 'package'])->name('package');
+Route::get('detail/{travelPackage:slug}', [App\Http\Controllers\PageController::class, 'detail'])->name('detail');
+
+
+Route::get('kontak-kami', [App\Http\Controllers\PageController::class, 'contact'])->name('contact');
+Route::post('kontak-kami', [App\Http\Controllers\PageController::class, 'getEmail'])->name('contact.email');
+
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::group(['middleware' => 'isAdmin', 'prefix' => 'admin', 'as' => 'admin.'], function() {
+        Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('posts', \App\Http\Controllers\Admin\PostController::class);
+        Route::resource('cars', \App\Http\Controllers\Admin\CarController::class);
+        Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class);
+        Route::resource('travel-packages', \App\Http\Controllers\Admin\TravelPackageController::class);
+        Route::resource('travel-packages.galleries', \App\Http\Controllers\Admin\GalleryController::class);
+    });
+    
 });

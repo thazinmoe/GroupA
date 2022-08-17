2cf5c974bc404b5cac188774667aec8022d1ac55
thazinmoe <thazinmoe1097@gmail.com>
2022-08-17 22:08:33 +0630

    cars-commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Auth::routes();
+
+Route::get('/', [App\Http\Controllers\PageController::class, 'home'])->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::group(['middleware' => 'isAdmin', 'prefix' => 'admin', 'as' => 'admin.'], function () {
+        Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('cars', \App\Http\Controllers\Admin\CarController::class);
+        Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class);
+        Route::get('customers', [CustomerApiController::class, 'index'])->name('customers');
+        Route::get('delete-customer/{id}', [CustomerApiController::class, 'destroy'])->name('destroy');
+    });
+});
+Route::get('customers', [CustomerApiController::class, 'index']);
+Route::get('add-customer/{travelPackage:slug}', [CustomerApiController::class, 'create'])->name('add-customer');
+Route::post('add-customer/{travelPackage:slug}', [CustomerApiController::class, 'store'])->name('add-customer');
+Route::get('/{id}/completed', [CustomerApiController::class, 'completed']);
+Route::get('edit-customer/{id}', [CustomerApiController::class, 'edit']);
+Route::put('update-customer/{id}', [CustomerApiController::class, 'update']);

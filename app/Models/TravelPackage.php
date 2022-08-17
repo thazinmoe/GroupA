e4e74fa53ca94a693e9ab3789f6e697ab842005a
Rosie Mary <laltnadi@gmail.com>
2022-08-17 11:35:33 +0630

    making table relationship

--- app/Models/TravelPackage.php
+++ app/Models/TravelPackage.php
@@ -2,14 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Car;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TravelPackage extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function cars(){
+        return $this->belongsTo(Car::class);
+    }
 
     public function category(){
         return $this->belongsTo(Category::class);

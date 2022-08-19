0bdf239367c9ed1fc4db20658005dd9b8b73200e
thazinmoe <thazinmoe1097@gmail.com>
2022-08-19 21:30:11 +0630

    changes

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -7,13 +7,6 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    function __construct()
-    {
-         $this->middleware('permission:dashboard-list|dashboard-create|dashboard-edit|dashboard-delete', ['only' => ['index','show']]);
-         $this->middleware('permission:dashboard-create', ['only' => ['create','store']]);
-         $this->middleware('permission:dashboard-edit', ['only' => ['edit','update']]);
-         $this->middleware('permission:dashboard-delete', ['only' => ['destroy']]);
-    }
     public function index(){
         return view('admin.dashboard.index');
     }

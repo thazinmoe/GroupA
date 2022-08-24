10dba34e610ff25acd36292230b65008ca86b9aa
Rosie Mary <laltnadi@gmail.com>
2022-08-24 22:20:40 +0630

    adding dashboardcontroller--

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -2,32 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Contracts\Services\DashboardServiceInterface;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
-{   
-    /**
-     * Dashboard interface
-     */
-    private $dashboardInterface;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct(DashboardServiceInterface $dashboardServiceInterface) {
-        $this->dashboardInterface = $dashboardServiceInterface;
-    }
-
+{
     public function index(){
-        $packages = $this->dashboardInterface->package();
-        $categories = $this->dashboardInterface->category();
-        $cars = $this->dashboardInterface->car();
-        $posts = $this->dashboardInterface->post();
-
-        return view('admin.dashboard.index',compact('packages','categories','cars','posts'));
+        return view('admin.dashboard.index');
     }
 }

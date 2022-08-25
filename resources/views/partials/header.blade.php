43888e17bf7a28647a061d74df34e570bb46c803
Theinhtike0 <theinhtikemm0@gmail.com>
2022-08-25 11:34:03 +0630

    add route

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,7 @@
         <div class="nav__menu" id="nav-menu">
           <ul class="nav__list">
             <li class="nav__item">
-              <a href="{{ route('viewHome') }}" class="nav__link {{ request()->is('/') ? ' active-link' : '' }}"">
+              <a href="{{ route('viewHome') }}" class="nav__link {{ request()->is('/') ? ' active-link' : '' }}">
                 <i class="bx bx-home-alt nav__icon"></i>
                 <span class="nav__name">Home</span>
               </a>
@@ -19,21 +19,21 @@
 
 
             <li class="nav__item">
-              <a href="">
+              <a href="{{ route('posts') }}" class="nav__link {{ request()->is('posts') ? ' active-link' : '' }}">
                 <i class="bx bx-book-alt nav__icon"></i>
                 <span class="nav__name">Blog</span>
               </a>
             </li>
 
             <li class="nav__item">
-              <a href="">
+              <a href="{{ route('package') }}" class="nav__link {{ request()->is('paket-travel') ? ' active-link' : '' }}">
                 <i class="bx bx-briefcase-alt nav__icon"></i>
                 <span class="nav__name">Package Travel</span>
               </a>
             </li>
 
             <li class="nav__item">
-              <a href="">
+              <a href="{{ route('contact') }}" class="nav__link {{ request()->is('contact') ? ' active-link' : '' }}">
                 <i class="bx bx-message-square-detail nav__icon"></i>
                 <span class="nav__name">Contact Us</span>
               </a>

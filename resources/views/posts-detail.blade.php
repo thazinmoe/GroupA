e935b411a5a9a235a3b3b5da76ba887b3f7bbc2e
eaindramyomyint <eaindramyo123@gmail.com>
2022-08-30 14:16:59 +0630

    blog page media query

--- resources/views/posts-detail.blade.php
+++ resources/views/posts-detail.blade.php
@@ -2,14 +2,9 @@
 
 @section('content')
 <main>
-
-      
-    <section class="blogDetail">
-
     
-      
-        <div class="postDetail">
-                
+    <section class="blogDetail">
+        <div class="postDetail">      
             <center><img src="{{ Storage::url($post->image) }}" alt=""/></center>      
             <div class="detailData">
                 <div class="data">
@@ -19,31 +14,29 @@
             </div>
             
             <div class="postBody">
-            <p>
-              {{ \Illuminate\Support\Str::limit($post->content, 200, '') }}
-              @if (strlen($post->content) > 200)
-                  <span id="dots">...</span>
-                  <span id="more">{{ substr($post->content, 100) }}</span>
-              @endif
-            </p>
+                <p>
+                {{ \Illuminate\Support\Str::limit($post->content, 100, '') }}
+                @if (strlen($post->content) > 100)
+                    <span id="dots">...</span>
+                    <span id="more">{{ substr($post->content, 100) }}</span>
+                @endif
+                </p>
 
-            <button onclick="myFunction()" id="myBtn" class="read-more">Read more</button>
-
-                    <!--{!! $post->content !!}-->
+                <button onclick="myFunction()" id="myBtn" class="read-more">Read more</button>
                     
             </div>
                   
         </div>          
             
-    </section><!-- Blog -->
+    </section><!-- Blog Detail-->
 
-     <section class="container">
+    <section class="container">
       <div class="txt-left"><h3 class="text-center mb-4">Posts</h3></div>
       
         <a href="{{ route('posts') }}" class="txt-right nav__link  {{ request()->is('posts') ? ' active-link' : '' }}">
                 <i class="bx bx-book-alt nav__icon"></i>
                 <span class="nav__name txt-right">All Posts</span>
-              </a>
+        </a>
       
         <div class="row slick_slider justify-content-center">
 
@@ -52,26 +45,12 @@
               <a href="{{ route('posts.show', $post) }}">
                 <div class="card text-center p-4">
                   <h4 class="title mb-2">{{ $post->title }}</h4>
-                  <span class="main-color"
-                    >Posting <span href="#">{{ $post->created_at->diffForHumans() }}</span></span
-                  >
+                  <span class="main-color">Posting <span href="#">{{ $post->created_at->diffForHumans() }}</span></span>
                   <div class="card-body p-3">
-                    <img
-                      src="{{ Storage::url($post->image) }}"
-                      alt=""
-                    />
+                    <img src="{{ Storage::url($post->image) }}" alt=""/>             
                     <p class="title-alt mt-5">
                       {{ $post->excerpt }}
                     </p>
-                    <!--<p>
-              {{ \Illuminate\Support\Str::limit($post->content, 200, '') }}
-              @if (strlen($post->content) > 200)
-                  <span id="dots">...</span>
-                  <span id="more">{{ substr($post->content, 100) }}</span>
-              @endif
-            </p>
-
-            <button onclick="myFunction()" id="myBtn" class="read-more">Read more</button>-->
                   </div>
                 </div>
               </a>
@@ -80,17 +59,9 @@
             @endforeach
 
         </div>
-      </section> <!--All Blog -->
+    </section> <!--All Blog -->
     </main>
 @endsection
 
-@push('style-alt')
-  <style>
-      .post-body p{
-        text-align: left ;
-        margin-top: 2rem;
-      }
-  </style>
-@endpush
 
 

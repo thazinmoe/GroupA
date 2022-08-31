0a1b6900f60b314b0f12f52c30b98383fa23a3c9
eaindramyomyint <eaindramyo123@gmail.com>
2022-08-31 10:33:48 +0630

    paginate add to admin/post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -59,9 +59,9 @@
                         @endforelse
                     </tbody>
                 </table>
-                
+                <div class="d-flex justify-content-center">
                     {!! $posts->links() !!}
-                
+                </div>
             </div>
         </div>
     
